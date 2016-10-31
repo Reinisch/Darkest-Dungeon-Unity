c2e54a1ad8598be19b878d006fd8138669eff539
Reinisch <kvv.bsuir@gmail.com>
2016-10-31 15:49:02 +0300

    hero roster scrollbar for touch input
    maybe should implement swipe instead in task #8

--- Assets/Scripts/UI/Panels/HeroRosterPanel.cs
+++ Assets/Scripts/UI/Panels/HeroRosterPanel.cs
@@ -29,6 +29,10 @@ public class HeroRosterPanel : MonoBehaviour, IPointerEnterHandler, IPointerExit
     void Awake()
     {
         rosterSlots = transform.FindChild("RosterScroll").FindChild("Viewport").FindChild("RosterSlots");
+#if !(UNITY_ANDROID || UNITY_IOS)
+        var scrollRect = GetComponentInChildren<ScrollRect>();
+        scrollRect.verticalScrollbar.gameObject.SetActive(false);
+#endif
     }
     void Start()
     {
