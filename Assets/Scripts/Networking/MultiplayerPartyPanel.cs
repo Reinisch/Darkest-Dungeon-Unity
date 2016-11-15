972ca71f7a7a01e0b4db6c99d8a8375572a65730
Reinisch <kvv.bsuir@gmail.com>
2016-11-15 21:37:15 +0300

    Multiplayer team composition UI

---
+++ Assets/Scripts/Networking/MultiplayerPartyPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
+
+public class MultiplayerPartyPanel : MonoBehaviour
+{
+    public Image eventOverlay;
+    public PartyCompositionPanel compositionPanel;
+
+    [SerializeField]
+    private List<MultiplayerPartySlot> partySlots;
+
+    public List<MultiplayerPartySlot> PartySlots
+    {
+        get
+        {
+            return partySlots;
+        }
+    }
+
+    void Awake()
+    {
+        for (int i = 0; i < PartySlots.Count; i++)
+            PartySlots[i].SlotId = i + 1;
+    }
+}
---
+++ Assets/Scripts/Networking/MultiplayerPartySlot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class MultiplayerPartySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image slotFrame;
+    public Image heroFrame;
+
+    public int SlotId { get; set; }
+    public Hero SelectedHero { get; set; }
+    public RectTransform RectTransform { get; private set; }
+
+    void Awake()
+    {
+        RectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (SelectedHero != null)
+            DarkestSoundManager.PlayOneShot("event:/ui/town/button_mouse_over");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (SelectedHero != null)
+            ToolTipManager.Instanse.Show(LocalizationManager.GetString("hero_class_name_" + SelectedHero.ClassStringId),
+                eventData, RectTransform, ToolTipStyle.FromTop, ToolTipSize.Small);
+    }
+}
