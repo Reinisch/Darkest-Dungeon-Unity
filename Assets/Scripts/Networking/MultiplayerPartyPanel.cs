40c8a6c2a33ac0f23d948c8b8e4265a968b817fa
Reinisch <kvv.bsuir@gmail.com>
2016-11-16 19:29:09 +0300

    Added multiplayer team compositions

--- Assets/Scripts/Networking/MultiplayerPartyPanel.cs
+++ Assets/Scripts/Networking/MultiplayerPartyPanel.cs
@@ -33,5 +33,33 @@ public class MultiplayerPartyPanel : MonoBehaviour
 
         for(int i = 0; i < PartySlots.Count; i++)
             PartySlots[i].UpdateHero(heroParty[i]);
+
+        compositionPanel.UpdateComposition(heroParty.Select(hero => hero.ClassStringId).ToList());
+    }
+
+    public void SwapNextHero(int slotIndex)
+    {
+        var heroPool = DarkestPhotonLauncher.Instanse.HeroPool;
+        int targetIndex = heroPool.IndexOf(PartySlots[slotIndex].SelectedHero);
+
+        while(true)
+        {
+            targetIndex++;
+            if (targetIndex >= heroPool.Count)
+                targetIndex = 0;
+
+            if (PartySlots.Find(slot => slot.SelectedHero == heroPool[targetIndex]))
+                continue;
+
+            break;
+        }
+
+        if (DarkestPhotonLauncher.CharacterWindow.gameObject.activeSelf)
+            if (DarkestPhotonLauncher.CharacterWindow.CurrentHero == PartySlots[slotIndex].SelectedHero)
+                DarkestPhotonLauncher.CharacterWindow.UpdateCharacterInfo(heroPool[targetIndex], true, true);
+
+        PartySlots[slotIndex].UpdateHero(heroPool[targetIndex]);
+
+        compositionPanel.UpdateComposition(PartySlots.Select(slot => slot.SelectedHero.ClassStringId).ToList());
     }
 }
